04cac5dcd024b8072e39bef788c1ce917f7d6b1e
Pat Maddox <pat@patmaddox.com>
2015-12-03 15:19:01 -0700

    my first model

--- feed/models.py
+++ feed/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    url = models.TextField()
+    title = models.TextField()
+    text = models.TextField()
