2662d1c087db7e3233cc19acad411de3f40c14c4
Pat Maddox <pat@patmaddox.com>
2015-12-03 15:32:46 -0700

    render articles in a feed format

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import feedgenerator
+from models import Article
 
 def index(request):
-    return HttpResponse("Hi.")
+    # articles = Article.objects.all()
+    context = {}
+    feed = feedgenerator.Rss201rev2Feed(
+        title = u"Pat's feed",
+        link = u"http://myfeed.patmaddox.com",
+        description = u"My super awesome custom feed",
+        language = u"en",
+    )
+
+    for article in Article.objects.all():
+        feed.add_item(
+            title=article.title,
+            link=article.url,
+            description=article.text
+        )
+
+    return HttpResponse(feed.writeString('utf-8'))
