9b5457bc97c256740a80a374fa264b2f1c257d20
Pat Maddox <pat@patmaddox.com>
2015-12-03 15:42:07 -0700

    add a method to fetch an article's title and text

--- feed/models.py
+++ feed/models.py
@@ -1,8 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import newspaper
 
 class Article(models.Model):
     url = models.TextField()
     title = models.TextField()
     text = models.TextField()
+
+    def fetch(self):
+        a = newspaper.Article(self.url, keep_article_html=True)
+        a.download()
+        a.parse()
+        self.title = a.title
+        self.text  = a.article_html
+        self.save()
